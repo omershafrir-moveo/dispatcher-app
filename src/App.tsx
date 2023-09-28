0d874635d15cf27f33ffba7930658cd377b150ea
Omer Shafrir <omershafrir@Omer-Shafrir.local>
2023-09-28 16:29:20 +0300

    took mockups out of recentSearches body

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,14 @@ import React, { useState } from "react";
 import DropDownMenu from "./components/DropdownMenu/DropDownMenu";
 import SearchBar from "./components/SearchBar/SearchBar/SearchBar";
 import RecentSearchesMenu from "./components/RecentSearches/RecentSearchesMenu/RecentSearchesMenu";
+import { RecentSearchesItem } from "./components/RecentSearches/RecentSearchesMenu/RecentSearchesMenu";
 
 function App() {
   return (
     <Container
       className="App"
       sx={{ backgroundColor: "#FFF2FF", height: "100vh" }}
-    >
-    </Container>
+    ></Container>
   );
 }
 
--- src/components/RecentSearches/RecentSearchesMenu/RecentSearchesMenu.tsx
+++ src/components/RecentSearches/RecentSearchesMenu/RecentSearchesMenu.tsx
@@ -9,39 +9,33 @@ import {
 import RecentSearchesItem from "../RecentSearchesItem/RecentSearchesItem";
 import { useState } from "react";
 
-const RecentSearchesMenu: React.FC<{
-  items: { key: number; text: string }[];
-}> = (props) => {
-  const [items, setItems] = useState(props.items);
+export type RecentSearchesItem = {
+  key: number;
+  text: string;
+};
 
-  const deleteMock = (itemToDelete: { key: number; text: string }) => {
-    console.log(`deleted ${itemToDelete.text}!`);
-    setItems((oldItems) =>
-      oldItems.filter((oldItem) => oldItem.key !== itemToDelete.key)
-    );
-  };
-  const clearMock = () => {
-    console.log("cleared all");
-    setItems([]);
-  };
-  const clickMock = (itemClicked: { key: number; text: string }) => {
-    console.log(`clicked ${itemClicked.text}!`);
-  };
+export type RecentSearchesMenuProps = {
+  items: RecentSearchesItem[];
+  handleClick: (item: RecentSearchesItem) => void;
+  handleDelete: (item: RecentSearchesItem) => void;
+  handleClear: () => void;
+};
 
+const RecentSearchesMenu: React.FC<RecentSearchesMenuProps> = (props) => {
   return (
     <Container>
       <HeaderContainer>
         <HeaderTitleContainer>RECENT SEARCHES</HeaderTitleContainer>
-        <ClearButton onClick={clearMock}>CLEAR</ClearButton>
+        <ClearButton onClick={props.handleClear}>CLEAR</ClearButton>
       </HeaderContainer>
-      {items && (
+      {props.items && (
         <ItemsList>
-          {items.map((item) => (
+          {props.items.map((item) => (
             <RecentSearchesItem
               key={item.key}
               text={item.text}
-              handleDelete={() => deleteMock(item)}
-              handleClick={() => clickMock(item)}
+              handleDelete={() => props.handleDelete(item)}
+              handleClick={() => props.handleClick(item)}
             />
           ))}
         </ItemsList>
